befae4ed3fd0348820a6d2edad3f6c610eea92b6
ImaWalkerr <75856607+ImaWalkerr@users.noreply.github.com>
2022-02-11 13:46:15 +0300

    Update project structure

--- game_muster/urls.py
+++ game_muster/urls.py
@@ -1,4 +1,4 @@
-"""GameMuster URL Configuration
+"""game_muster URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('GameMusterApp.urls')),
+    path('', include('game_muster_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
--- game_muster/wsgi.py
+++ game_muster/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GameMuster project.
+WSGI config for game_muster project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GameMuster.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_muster.settings')
 
 application = get_wsgi_application()
