5b849d74fb0995e4e8ac0bb190320e0297d0f172
ImaWalkerr <75856607+ImaWalkerr@users.noreply.github.com>
2022-03-29 19:16:56 +0300

    Some changes for dev branch
    Remove sqlite3 for heroku, added back postgres db for dev

--- game_muster/settings.py
+++ game_muster/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'game_muster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres_db',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '65432',
     },
 }
 
