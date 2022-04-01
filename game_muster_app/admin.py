4cd9bc83935d9c85dbfdf5bdc6d741a6df322384
ImaWalkerr <75856607+ImaWalkerr@users.noreply.github.com>
2022-04-01 16:13:50 +0300

    Task 5
    Added models

--- game_muster_app/admin.py
+++ game_muster_app/admin.py
@@ -1,5 +1,49 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(GameUser)
-admin.site.register(UserFavoriteGames)
+
+@admin.register(GameUser)
+class GameUserAdmin(admin.ModelAdmin):
+    fields = (
+        ('user', 'birthday', 'gender'), 'email_verify'
+    )
+    list_display = ('user', 'email_verify')
+    list_display_links = ('user',)
+
+
+@admin.register(UserFavoriteGames)
+class UserFavoriteGamesAdmin(admin.ModelAdmin):
+    fields = ('owner', 'game_id')
+    list_display = ('owner', 'game_id')
+    list_display_links = ('owner',)
+
+
+@admin.register(Platforms)
+class PlatformsAdmin(admin.ModelAdmin):
+    fields = ('platform_id', 'platform_name')
+    list_display = ('platform_id', 'platform_name')
+    list_display_links = ('platform_name',)
+
+
+@admin.register(Genres)
+class GenresAdmin(admin.ModelAdmin):
+    fields = ('genre_id', 'genre_name')
+    list_display = ('genre_id', 'genre_name')
+    list_display_links = ('genre_name',)
+
+
+@admin.register(Games)
+class GamesAdmin(admin.ModelAdmin):
+    fields = (
+        ('game_id', 'game_name', 'release_dates'), 'game_summary', ('game_genres', 'game_platforms'), 'cover_url',
+        ('rating', 'rating_count', 'aggregated_rating', 'aggregated_rating_count')
+    )
+    list_display = ('game_id', 'game_name')
+    list_display_links = ('game_name',)
+
+
+@admin.register(ScreenShots)
+class ScreenShotsAdmin(admin.ModelAdmin):
+    fields = ('game', 'screenshot_url')
+    list_display = ('game', 'screenshot_url')
+    list_display_links = ('game',)
--- game_muster_app/models.py
+++ game_muster_app/models.py
@@ -32,12 +32,102 @@ class UserFavoriteGames(models.Model):
     """
     User wishlist
     """
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner', related_name='user_favorite_game')
-    game_id = models.IntegerField(default=0, verbose_name='User games')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Owner', related_name='owner_favorites')
+    game_id = models.ForeignKey(
+        'Games', on_delete=models.CASCADE, verbose_name='User games', related_name='user_favorite_games'
+    )
 
     def __str__(self):
         return f"{self.owner}"
 
     class Meta:
+        db_table = 'user_wishlist'
         verbose_name = 'User wishlist'
         verbose_name_plural = 'Users wishlists'
+
+
+class Platforms(models.Model):
+    """
+    Platforms model
+    """
+    platform_id = models.IntegerField(default=0, verbose_name='Platform id')
+    platform_name = models.CharField(max_length=124, verbose_name='Platform name')
+
+    def __str__(self):
+        return f"{self.platform_name}"
+
+    class Meta:
+        db_table = 'platforms'
+        verbose_name = 'Platform'
+        verbose_name_plural = 'Platforms'
+
+
+class Genres(models.Model):
+    """
+    Genres model
+    """
+    genre_id = models.IntegerField(default=0, verbose_name='Genre id')
+    genre_name = models.CharField(max_length=124, verbose_name='Genre name')
+
+    def __str__(self):
+        return f"{self.genre_name}"
+
+    class Meta:
+        db_table = 'genres'
+        verbose_name = 'Genre'
+        verbose_name_plural = 'Genres'
+
+
+class Games(models.Model):
+    """
+    Games model
+    """
+    game_id = models.IntegerField(default=0, verbose_name='Game Id from Igdb.com')
+    game_name = models.CharField(max_length=124, verbose_name='Game name')
+    game_summary = models.TextField(max_length=1024, verbose_name='Game summary')
+    game_genres = models.ManyToManyField(
+        Genres, verbose_name='Game genres', related_name='game_genres'
+    )
+    game_platforms = models.ManyToManyField(
+        Platforms, verbose_name='Game platforms', related_name='game_platforms'
+    )
+    cover_url = models.CharField(max_length=256, verbose_name='Game cover')
+    release_dates = models.DateTimeField(null=True, default=None, verbose_name='Game release dates')
+    rating = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True, default=None, verbose_name='Game rating'
+    )
+    rating_count = models.IntegerField(null=True, default=None, verbose_name='Game rating count')
+    aggregated_rating = models.DecimalField(
+        max_digits=4, decimal_places=2, null=True, default=None, verbose_name='Game aggregated rating'
+    )
+    aggregated_rating_count = models.IntegerField(
+        null=True, default=None, verbose_name='Game aggregated rating count'
+    )
+    favorite_games = models.ManyToManyField(
+        User, through='UserFavoriteGames', verbose_name='game_in_users_favorites',
+        related_name='game_in_users_favorites'
+    )
+
+    def __str__(self):
+        return f"{self.game_name}"
+
+    class Meta:
+        db_table = 'games'
+        verbose_name = 'Games catalog'
+        verbose_name_plural = 'Games catalogs'
+
+
+class ScreenShots(models.Model):
+    """
+    Screenshots model
+    """
+    game = models.ForeignKey(Games, on_delete=models.CASCADE, verbose_name='Game', related_name='game_screenshots')
+    screenshot_url = models.CharField(max_length=256, verbose_name='Game screenshot')
+
+    def __str__(self):
+        return f"{self.game}"
+
+    class Meta:
+        db_table = 'screenshots'
+        verbose_name = 'Screenshot'
+        verbose_name_plural = 'Screenshots'
